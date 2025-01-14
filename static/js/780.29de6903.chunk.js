589fde0df049d8cb0631b32832f9a6752b7e33ad
Mamadou Moctar BAH <moctarbah73@gmail.com>
2025-01-14 15:22:38 +0000

    Updates

---
+++ static/js/780.29de6903.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmamipastries=self.webpackChunkmamipastries||[]).push([[780],{780:(s,e,a)=>{a.r(e),a.d(e,{default:()=>t});const t={}}}]);
+//# sourceMappingURL=780.29de6903.chunk.js.map
